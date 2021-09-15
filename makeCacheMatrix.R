0977a127b8bc37b36620fedaa1cc78fb9daf555b
rlaehdjs279 <88371783+rlaehdjs279@users.noreply.github.com>
2021-09-16 08:57:46 +0900

    Update makeCacheMatrix.R
    add more command lines

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,27 @@
-makeCacheMatrix <- function(x = matrix()) {
-  
-  inv <- NULL
+
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
   set <- function(y) {
     x <<- y
-    inv <<- NULL
+    s <<- NULL
   }
   get <- function() x
-  setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+##
+## Same here, changed "mean" to "solve" and "m" to "s"
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
