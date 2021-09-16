33b6fc62f4d851e766198ac56ad0a42fd484df00
rlaehdjs279 <88371783+rlaehdjs279@users.noreply.github.com>
2021-09-16 09:16:17 +0900

    Update makeCacheMatrix.R
    whole revision

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,27 +1,14 @@
 
-makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
-  s <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+makeCacheMatrix <- function(x = matrix()) {
+
+inv <- NULL
+set <- function(y) {
+x <<- y
+inv <<- NULL
 }
-##
-## Same here, changed "mean" to "solve" and "m" to "s"
-cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
-  }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+get <- function() x
+setinv <- function(inverse) inv <<- inverse
+getinv <- function() inv
+list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
+
